d4325e7b490401475e624854ec15dac477586700
Barak Horowitz <bahorcse@gmail.com>
2024-05-21 16:52:12 -0700

    added base login check functionality, waiting on database configuration

--- backend/index.mjs
+++ backend/index.mjs
@@ -11,6 +11,11 @@ const login_dir = 'pages/login_page/'
 const login_index_dir = 'pages/login_index_page/'
 const signup_dir = 'pages/signup_page/'
 
+
+const stored_username = 'username'
+
+const stored_password = 'password'
+
 // DB CONNECTION
 async function main() {
   const client = new MongoClient(db_url)
@@ -45,6 +50,10 @@ const port = 4000;
 app.use(express.json());
 app.use(cors());
 
+// Middleware to parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
+
+
 
 app.listen(port, () => {
   console.log('stargazer listening on port ' + port)
@@ -66,6 +75,21 @@ app.get('/login_index', (req, res) => {
 app.get('/signup', (req, res) => {
   console.log('RECIEVED SIGNUP PAGE REQUEST')
   res.sendFile(signup_dir + 'signup.html', {root: '../source'} )
+})
+
+app.post('/login_attempt', (req, res) => {
+  console.log("RECIEVED LOGIN ATTEMPT!")
+  const {rec_username, rec_password} = req.body
+  console.log('username attempted to login with ' + rec_username)
+  console.log('password attempted to login with ' + rec_password)
+  if(rec_username == stored_username && rec_password == stored_password) {
+    console.log('SUCCESSFUL LOGIN!')
+    res.send('LOGIN SUCCESSFUL')
+  } else {
+    console.log('FAILED LOGIN!')
+    res.send('LOGIN FAILED')
+  }
+  
 })
 
 
