f7d8ce5286007ce58b00820916abc45c737357a0
Barak Horowitz <bahorcse@gmail.com>
2024-05-21 16:25:39 -0700

    hooked up central server to a mongoDB cluster

--- backend/index.mjs
+++ backend/index.mjs
@@ -2,10 +2,42 @@
 import express from "express";
 import cors from "cors";
 
+// MONGO VARIABLES
+import { MongoClient } from 'mongodb';
+const db_url = 'mongodb+srv://bahorowitz:HxfQTvBgarDEoXTE@stargazercluster.j46iehf.mongodb.net/?retryWrites=true&w=majority&appName=stargazerCluster'
+
+// PAGE DIRECTORY VARIABLES
 const login_dir = 'pages/login_page/'
 const login_index_dir = 'pages/login_index_page/'
 const signup_dir = 'pages/signup_page/'
 
+// DB CONNECTION
+async function main() {
+  const client = new MongoClient(db_url)
+  try {
+    await client.connect()
+    await listDatabases(client)
+  } catch (e) {
+    console.error(e)
+  }
+  finally {
+    await client.close();
+  }
+}
+
+main().catch(console.error)
+
+async function listDatabases(client){
+  const databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+
+
+
+// SERVER SETUP
 // Set up node js and routes
 const app = express();
 const port = 4000;
