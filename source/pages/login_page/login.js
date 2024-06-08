e5ce0c68074fec30e6b1e58ed6998773e0430c13
pramodya-rajapakse <102259936+pramodya-rajapakse@users.noreply.github.com>
2024-06-08 16:04:06 -0700

    reformat with prettier

--- source/pages/login_page/login.js
+++ source/pages/login_page/login.js
@@ -1,83 +1,31 @@
-// document.getElementById('togglePassword').addEventListener('click', function() {
-//     var passwordField = document.getElementById('rec_password');
-//     var type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
-//     passwordField.setAttribute('type', type);
-//     this.textContent = type === 'password' ? '👁️' : '🙈'; // Optional: Toggle icon
-// });
-
-
-
-
-// document.getElementById('submitButton').addEventListener('click', function(){
-//     const cookies = document.cokie;
-    
-    
-//     if (cookies == 'loggedin=true'){
-//         fetch('/selection/page')
-//         .then(response => {
-//             if (response.ok) {
-//             // If the response is successful, navigate to the new page
-//             window.location.href = '/selection/page';
-//             } else {
-//             // Handle error if the response is not successful
-//             console.error('Failed to fetch:', response.statusText);
-//             // Optionally show an error message or perform other actions
-//             }
-//         })
-//         window.location.href = "/selection/page";
-//         console.log('logged in is false');
-//     }else {
-//         popup.style.display = 'inline-block';
-//         overlay.style.display = 'inline-block';
-//     }
-
-    
-// });
-
-
-// document.getElementById('popupButton').addEventListener('click', function(){
-//     popup.style.display = 'none';
-//     overlay.style.display = 'none';
-//     const username = document.getElementById('rec_username');
-//     username.value = ''; // Clear the input field
-//     const pass = document.getElementById('rec_password');
-//     pass.value = ''; // Clear the input field
-// });
-
 // // testUser1 testPassOne
-document.getElementById('myForm').addEventListener('submit', function(event){
-    event.preventDefault();
+document.getElementById("myForm").addEventListener("submit", function (event) {
+  event.preventDefault();
 
+  let formData = new FormData(this);
+  console.log(formData);
 
-    
+  let xhr = new XMLHttpRequest();
 
-    let formData = new FormData(this);
-    console.log(formData);
+  xhr.open("POST", "/login/attempt", true);
+  xhr.setRequestHeader("Content-Type", "application/json");
 
-    let xhr = new XMLHttpRequest();
-
-    xhr.open('POST', '/login/attempt', true);
-    xhr.setRequestHeader("Content-Type", 'application/json');
-
-    xhr.onload = function () {
-        if(xhr.status === 200){
-            console.log("Successful login");
-            const errorMsg = document.getElementById('errorMsg');
-            errorMsg.style.display = 'none';
-            window.location.href = "/selection/page";
-        } else {
-            const errorMsg = document.getElementById('errorMsg');
-            errorMsg.style.display = 'block';
-            console.error("Error:", xhr.statusText);
-        }
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      console.log("Successful login");
+      const errorMsg = document.getElementById("errorMsg");
+      errorMsg.style.display = "none";
+      window.location.href = "/selection/page";
+    } else {
+      const errorMsg = document.getElementById("errorMsg");
+      errorMsg.style.display = "block";
+      console.error("Error:", xhr.statusText);
     }
+  };
 
-    xhr.onerror = function () {
-        console.error('Network Error');
-    }
+  xhr.onerror = function () {
+    console.error("Network Error");
+  };
 
-    xhr.send(JSON.stringify(Object.fromEntries(formData)));
-
-    
+  xhr.send(JSON.stringify(Object.fromEntries(formData)));
 });
-    
--- source/pages/signup_page/signup.js
+++ source/pages/signup_page/signup.js
@@ -1,56 +1,50 @@
-let password = document.getElementById("new_password")
+let password = document.getElementById("new_password");
 let conf_password = document.getElementById("new_password_conf");
 
-function confirmPassword(){
+function confirmPassword() {
   if (password.value != conf_password.value) {
     conf_password.setCustomValidity("Passwords Do Not Match");
   } else {
-    conf_password.setCustomValidity('');
+    conf_password.setCustomValidity("");
   }
 }
 
 password.onchange = confirmPassword;
 conf_password.onkeyup = confirmPassword;
 
-document.getElementById('myForm').addEventListener('submit', function(event){
+document.getElementById("myForm").addEventListener("submit", function (event) {
   event.preventDefault();
-
-
-  
 
   let formData = new FormData(this);
   console.log(formData);
 
   let xhr = new XMLHttpRequest();
 
-  xhr.open('POST', '/signup/attempt', true);
-  xhr.setRequestHeader("Content-Type", 'application/json');
+  xhr.open("POST", "/signup/attempt", true);
+  xhr.setRequestHeader("Content-Type", "application/json");
 
   xhr.onload = function () {
-      if(xhr.status === 201){
-          console.log("Successful login");
-          const errorMsg = document.getElementById('takenUsername');
-          errorMsg.style.display = 'none';
-          const ukProb = document.getElementById('unknownProblem');
-          ukProb.style.display = 'none';
-          window.location.href = "/selection/page";
-      } else if(xhr.status === 200){
-          const takenUser = document.getElementById('takenUsername');
-          takenUser.style.display = 'block';
-          console.error("Error:", xhr.statusText);
-      }else if(xhr.status === 204){
-        const ukProb = document.getElementById('unknownProblem');
-        ukProb.style.display = 'block';
-        console.error("Error:", xhr.statusText);
+    if (xhr.status === 201) {
+      console.log("Successful login");
+      const errorMsg = document.getElementById("takenUsername");
+      errorMsg.style.display = "none";
+      const ukProb = document.getElementById("unknownProblem");
+      ukProb.style.display = "none";
+      window.location.href = "/selection/page";
+    } else if (xhr.status === 200) {
+      const takenUser = document.getElementById("takenUsername");
+      takenUser.style.display = "block";
+      console.error("Error:", xhr.statusText);
+    } else if (xhr.status === 204) {
+      const ukProb = document.getElementById("unknownProblem");
+      ukProb.style.display = "block";
+      console.error("Error:", xhr.statusText);
     }
-  }
+  };
 
   xhr.onerror = function () {
-      console.error('Network Error');
-  }
+    console.error("Network Error");
+  };
 
   xhr.send(JSON.stringify(Object.fromEntries(formData)));
-
-  
 });
-  
